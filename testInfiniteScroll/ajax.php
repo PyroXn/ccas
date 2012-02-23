61aa8cd3485854ba11cd12cff725423599a0791e
PyroXn <pierre.charrasse@gmail.com>
2012-02-23 14:35:12 +0100

    test d'infiniteScroll

---
+++ testInfiniteScroll/ajax.php
@@ -0,0 +1,21 @@
+<?php
+
+include('../config.php');
+$retour = '';
+$foyers = Doctrine_Core::getTable('Foyer');
+$i = $_GET['last'] +1;
+foreach ($foyers->searchByLimitOffset(100, $_GET['last'])->execute() as $foyer) {
+    if ($i % 2 == 0) {
+        $retour .= '<li class="pair foyer" id="' . $i . '">';
+    } else {
+        $retour .= '<li class="impair foyer" id="' . $i . '">';
+    }
+    $retour .= '
+                            <a href="#">
+                                <span class="label">' . $foyer->nom . ' ' . $foyer->prenom . ' ' . $foyer->id . '</span>
+                            </a>
+                        </li>';
+    $i++;
+}
+echo $retour;
+?>
