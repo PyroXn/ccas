714363f2b8c7816797160a7f80b6265fb42cf9d7
Florian <florian.janson@gmail.com>
2012-02-23 10:51:32 +0100

    ajout class user

---
+++ modeles/User.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class User extends Doctrine_Record {
+
+    public function setTableDefinition() {
+        $this->setTableName('user');
+        $this->hasColumn('id', 'integer', 8, array('primary' => true,
+            'autoincrement' => true));
+        $this->hasColumn('login', 'string', 50);
+        $this->hasColumn('password', 'string', 80);
+        $this->hasColumn('level', 'integer', 2);
+        
+    }
+
+}
+?>
---
+++ modeles/UserTable.class.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once dirname(__FILE__).'/modeles/User.class.php';
+
+class UserTable extends Doctrine_Table
+{
+
+}
+?>
