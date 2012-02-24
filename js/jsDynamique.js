c663419d0e2befc03e8c664457f38ec87a8c83fd
PyroXn <pierre.charrasse@gmail.com>
2012-02-24 15:17:39 +0100

    css du current

--- js/jsDynamique.js
+++ js/jsDynamique.js
@@ -2,12 +2,12 @@
  * ETANT DONNE QUE L'AJAX NE RECHARGE PAS TOUTE LA PAGE LE JQUERY N'EST EFFECTIF
  * QUE SUR LA PAGE INITIAL */
 $(function() {
+    /* retourne l'element sur lequel on clique */
     $('#list_individu > li').click(function() {
-        console.log("ONCLICK");
-        $(this).css({
-            "background-color":"black"
-        });
-        $(this).addClass("current");
+        console.log('ONCLICK');
+        /* recherche dans les enfants de l'id list_individu une class current */
+        $('#list_individu').children('.current').removeClass('current');
+        $(this).addClass('current');
     });  
     
 });
