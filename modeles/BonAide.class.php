3f485f8f4e44bfd5656623072f19c4fda1efb6ed
BaD <jerome.wautrin@mydevhouse.com>
2012-03-21 18:01:38 +0100

    maj aide interne + bon

---
+++ modeles/BonAide.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class AideExterne extends Doctrine_Record {
+
+    public function setTableDefinition() {
+        $this->setTableName('aideexterne');
+
+        $this->hasColumn('id', 'integer', 8, array('primary' => true,
+            'autoincrement' => true));
+        $this->hasColumn('idAideInterne', 'integer', 20);
+        $this->hasColumn('idInstruct', 'integer', 20);
+        $this->hasColumn('idInstruct', 'integer', 20);
+        $this->hasColumn('idTypeAide', 'integer', 20);
+        $this->hasColumn('dateRemisePrevu', 'integer', 20);
+        $this->hasColumn('dateRemiseEffective', 'integer', 20);
+        $this->hasColumn('montant', 'float', null, array('type' => 'float', 'default' => 0));
+        $this->hasColumn('commentaire', 'string',250);
+
+    }
+
+    public function setUp() {
+        $this->hasOne('aideInterne as aideInterne', array(
+            'local' => 'idAideInterne',
+            'foreign' => 'id'
+                )
+        );
+    }
+}
+
+
+?>
---
+++ modeles/BonAideTable.class.php
@@ -0,0 +1,9 @@
+<?php
+// On inclut le modèle «News ».
+require_once dirname(__FILE__).'/AideExterne.class.php';
+
+class AideExterneTable extends Doctrine_Table
+{
+    
+}
+?>
