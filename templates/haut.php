a7aceac4e1600d5d0b7aabc8eaeedc862cb7b72a
BaD <jerome.wautrin@mydevhouse.com>
2012-04-03 10:37:20 +0200

    datepicker !

--- templates/haut.php
+++ templates/haut.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" type="text/css" href="./templates/tipsy.css" media="screen" />
         <link rel="stylesheet" type="text/css" href="./templates/glDatePicker.css" media="screen" />
         <link rel="stylesheet" type="text/css" href="./templates/form.css" media="screen" />
-        
+        <link rel="stylesheet" type="text/css" href="./templates/ui-lightness/jquery-ui-1.8.18.custom.css" media="screen" />
         
 <!--         Bootstrap CSS Toolkit styles 
 <link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap.min.css" />
