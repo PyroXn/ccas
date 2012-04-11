5dbfab29274d1defde97930bd18e4ea21c302128
Zozor <florian.janson@mydevhouse.com>
2012-04-11 21:52:42 +0200

    suite

--- lib/PDF/generateRapport.php
+++ lib/PDF/generateRapport.php
@@ -7,5 +7,5 @@
     require_once(dirname(__FILE__).'/html2pdf/html2pdf.class.php');
     $html2pdf = new HTML2PDF('P','A4','fr');
     $html2pdf->WriteHTML($content);
-    $html2pdf->Output('exemple.pdf', 'F');
+    $html2pdf->Output($chemin.'/'.$individu->id.'/RapportSocial.pdf', 'F');
 ?>
