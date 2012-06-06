606824eb9e08ad049bf9aee06e154d80a5bda1b8
PyroXn <pierre.charrasse@gmail.com>
2012-06-06 09:46:05 +0200

    bug sur les combo qui ne se remplissais pas

--- lib/config.php
+++ lib/config.php
@@ -42,6 +42,9 @@ function isAuthorized($level) {
  * return vrai si == null, 0 ou ''
  */
 function verifieValeurNull($val) {
+    if (is_string($val)) {
+        return $val == null || $val == '';
+    }
     return $val == null || $val == '' || $val == 0;
 }
 
