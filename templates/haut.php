fd1a2d9bffd9fbb5e71a1582fead3ee422784a46
Zozor <florian.janson@mydevhouse.com>
2012-06-30 17:37:39 +0200

    correction table statique

--- templates/haut.php
+++ templates/haut.php
@@ -2,7 +2,7 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns ="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
     <head>
-        <title>ccas</title>
+        <title>MDH CCAS v1.0</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link rel="stylesheet" type="text/css" href="./templates/navigationbar.css" media="screen" />
         <link rel="stylesheet" type="text/css" href="./templates/ccas.css" class="switchable2" />
